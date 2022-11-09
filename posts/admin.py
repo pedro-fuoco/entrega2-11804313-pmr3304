544b1bc7d989d5fffde880d667f9bd582dc50da1
pedro-fuoco <pedrofuoco6@gmail.com>
2022-11-09 18:25:32 -0300

    rename da classe de comentários3

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post, Review
+from .models import Post, Comment
 
 admin.site.register(Post)
-admin.site.register(Review)
+admin.site.register(Comment)
